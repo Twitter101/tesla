dc2155e2202769dfae57c632c9934e5a13ad7829
Usher Aba <abasimon90@gmail.com>
2022-11-30 14:47:21 -0500

    web app created

--- app.java
+++ app.java
@@ -1,5 +1,5 @@
-This is a project for twitter
-It covered the interface of the news media
-Let it not be a hard core
-Author: Simon Aba
+#This is a project for twitter
+#It covered the interface of the news media
+#Let it not be a hard core
+#Author: Simon Aba
 
